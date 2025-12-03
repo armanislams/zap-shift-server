80640142bb7f8737b4427e9faf834c219159cd0f
ArmanIslamTC <armanislam.ais@gmail.com>
2025-12-04 00:22:28 +0800

    added jwt verify

---
+++ index.js
@@ -0,0 +1,251 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+const port = process.env.PORT || 3000
+const crypto = require('crypto');
+const admin = require("firebase-admin");
+const serviceAccount = require("./zap-shift.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+function generateTrackingId() {
+  const prefix = 'PRCL'
+  const date = new Date().toISOString().slice(0,10).replace(/-/g,'') //YYYYMMDD 
+  const random = crypto.randomBytes(3).toString('hex').toUpperCase();//6-char random hex
+  return`${prefix}-${date}-${random}`
+}
+//middleware
+app.use(express.json())
+app.use(cors())
+
+const verifyFirebaseToken = async (req, res, next) => {
+  
+  console.log("header in middleware", req.headers.authorization);
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).send({message: 'unauthorized access'})
+  }
+  try {
+    const idToken = token.split(' ')[1]
+    const decoded = await admin.auth().verifyIdToken(idToken)
+    console.log('decoded token', decoded);
+    req.decoded_email = decoded.email
+    next()
+  }
+  catch(err) {
+    return res.status(401).send({message: 'unauthorized access'})
+  }
+}
+
+//mongodb
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xbf1ip3.mongodb.net/?appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      const db = client.db('zap-shift')
+    const parcelsCollection = db.collection('parcels')
+    const userCollection = db.collection('users')
+    const paymentCollection = db.collection('payments')
+
+
+    ///user apis
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      user.role = 'user';
+      user.createdAt = new Date();
+      const email = user.email
+      const userExist = await userCollection.findOne({email})
+      if (userExist){
+        return res.send({message: 'user exist'})
+      }
+      const result = await userCollection.insertOne(user)
+      res.send(result)
+    })
+      
+      // parcel api
+      app.get('/parcels',async (req, res) => {
+          const query = {}
+          const { email} = req.query;
+          //parcels by email
+          if (email) {
+              query.senderEmail = email;
+          }
+          const options ={sort: {createdAt: -1}}
+          const cursor = parcelsCollection.find(query, options);
+        const result =  await cursor.toArray()
+        res.send(result)
+      })
+      app.get('/parcels/:id', async (req, res) => {
+          const id = req.params.id
+          const query = {_id: new ObjectId(id)}
+          const result = await parcelsCollection.findOne(query)
+          res.send(result)
+      })
+      app.post('/parcels',async (req, res) => {
+          const parcel = req.body
+            //parcel create time
+          parcel.createdAt = new Date()
+          
+          const result = await parcelsCollection.insertOne(parcel)
+          res.send(result)
+      })
+      app.delete('/parcels/:id', async (req, res) => {
+          const id = req.params.id
+          const query = {_id: new ObjectId(id)}
+          const result = await parcelsCollection.deleteOne(query)
+          res.send(result)
+      })
+
+    // checkout API
+    app.post('/create-checkout-session', async (req, res) => {
+      const paymentInfo = req.body
+      const amount = parseInt(paymentInfo.cost) * 100
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            // Provide the exact Price ID (for example, price_1234) of the product you want to sell
+            price_data: {
+              currency: "USD",
+              unit_amount: amount,
+              product_data: {
+                name: paymentInfo.parcelName,
+              },
+            },
+            quantity: 1,
+          },
+        ],
+        mode: "payment",
+        customer_email: paymentInfo.senderEmail,
+        metadata: {
+          parcelId: paymentInfo.parcelId,
+          parcelName: paymentInfo.parcelName,
+        },
+        success_url: `${process.env.SITE_URL}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${process.env.SITE_URL}/dashboard/payment-cancelled`,
+      });
+      console.log(session);
+      res.send({url: session.url})
+      
+    })
+    app.patch('/payment-success', async(req, res) => {
+      const sessionId = req.query.session_id
+      const session = await stripe.checkout.sessions.retrieve(sessionId) 
+      // console.log('session retrieve', session);
+
+      const transactionId = session.payment_intent
+      const query = {
+        transactionId : transactionId 
+      }
+      const paymentExist = await paymentCollection.findOne(query)
+
+      if (paymentExist) {
+        return res.send({message: 'already exist', transactionId, trackingId: paymentExist.trackingId})
+      }
+      console.log(paymentExist);
+      
+
+      const trackingId = generateTrackingId()
+
+      if (session.payment_status === 'paid') {
+        const id = session.metadata.parcelId
+        const query = {_id: new ObjectId(id)}
+        const update = {
+          $set: {
+            paymentStatus: "paid",
+            trackingId: trackingId
+          },
+        };
+        const result = await parcelsCollection.updateOne(query, update)
+        const payment = {
+          amount: session.amount_total / 100,
+          currency: session.currency,
+          email: session.customer_email,
+          parcelId: session.metadata.parcelId,
+          parcelName: session.metadata.parcelName,
+          transactionId: session.payment_intent,
+          paymentStatus: session.payment_status,
+          paidAt: new Date(),          
+          trackingId: trackingId
+        };
+        if (session.payment_status === 'paid') {
+          const resultPayment = await paymentCollection.insertOne(payment)
+          res.send({
+            success: true,
+            modifyParcel: result,
+            paymentInfo: resultPayment,
+            trackingId: trackingId,
+            transactionId: session.payment_intent,
+          });
+        }
+        
+      }
+      res.send({success: false})
+      
+    })
+    // app.get("/payments", async (req, res) => {
+    //   const query = {};
+    //   const { email } = req.query;
+    //   //parcels by email
+    //   if (email) {
+    //     query.senderEmail = email;
+    //   }
+    //   const options = { sort: { createdAt: -1 } };
+    //   const cursor = paymentCollection.find(query, options);
+    //   const result = await cursor.toArray();
+    //   res.send(result);
+    // });
+    //payment api
+    app.get('/payments',verifyFirebaseToken, async (req, res) => {
+      const email = req.query.email
+      // console.log('headers',req.headers);
+      
+      const query = {}
+      if (email) {
+        query.email = email
+        //check email
+        if (email !== req.decoded_email) {
+          return res.status(403).send({message: 'forbidden access'})
+        }
+      }
+      const options = {sort:{paidAt: -1}}
+      const cursor = paymentCollection.find(query,options)
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the
+    //  client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('ZapShift Server')
+})
+
+app.listen(port, () => {
+    console.log('port running on',port);
+    
+})
